8c3f2397bc3ebf9512f7026b9dc9856cfb53669b
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-25 12:29:24 +0200

    exercice 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exercice 1</title>
+  </head>
+  <body>
+
+    <?php
+    $name = 'cedric';  //création de la variable, qui est égale a cedric
+    echo 'Bonjour, je m\'appelle ' . $name ;
+    ?>
+
+  </body>
+</html>
